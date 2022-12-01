95b511a38e1f2dfabe6e21edbafe94cf09967f83
blind675 <catalin.bora@gmail.com>
2022-12-01 10:33:02 +0100

    added comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+// De retinut ca importul CSS-ului in acest mod este posibil cu ajutorul lui Webpack.
 import "./App.css";
 import "./styles.css";
+// In cazul in care componenta este exportata fara default, o importam folosind acolade.
 import { Navigation, ExtraCmp } from "./components/Navigation";
+// In cazul in care componenta este exportata default, o importam fara acolade.
 import UserClass from "./components/UserClass";
 import User from "./components/User";
 
@@ -8,8 +11,9 @@ function App() {
   return (
     <div className="App">
       <ExtraCmp />
+      {/* Instantiem (cream) componenta Navigation. */}
       <Navigation />
-      <UserClass nume={"XAA-12"} varsta={29} />
+      {/* Instantiem (cream) componenta User, pasandu-i valorile pentru prop-uri. */}
       <User nume={"Vasile"} varsta={20} />
       <User nume={"Ana"} varsta={23} />
       <User
@@ -21,6 +25,8 @@ function App() {
           console.log("Salut din callback");
         }}
       />
+      {/* Chiar daca este o clasa, componenta este creata la fel */}
+      <UserClass nume={"XAA-12"} varsta={29} />
     </div>
   );
 }
